a615c612b433e6cab01c0ee2ea9f7ebbfb6fa79b
HadleySo <71105018+HadleySo@users.noreply.github.com>
2025-11-17 04:02:47 +0000

    Fixing SESv2 Credentials (#10)
    * Setting credentials on SES client with Viper
    
    * Setting AWS region with Viper

--- src/mailer/invite.go
+++ src/mailer/invite.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"github.com/hadleyso/netid-activate/src/emailTemplate"
@@ -19,7 +20,16 @@ func HandleSendInvite(email string) error {
 
 	// 1. Load AWS SDK configuration (uses env vars)
 	ctx := context.Background()
-	cfg, err := config.LoadDefaultConfig(ctx)
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion(viper.GetString("AWS_REGION")),
+		config.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				viper.GetString("AWS_ACCESS_KEY_ID"),
+				viper.GetString("AWS_SECRET_ACCESS_KEY"),
+				"",
+			),
+		),
+	)
 	if err != nil {
 		log.Println("HandleSendInvite() unable to load AWS config")
 		return err
